13ac7d4116a43b95bb79c045972072cbb0ff6ea4
stanley1208 <54892792+stanley1208@users.noreply.github.com>
2022-08-30 12:46:31 +0800

    Add files via upload

---
+++ Talker.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Talker : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // say something
+        Debug.Log("I am scarlet witch");
+    }
+
+   
+}
